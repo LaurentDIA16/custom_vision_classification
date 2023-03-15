5d811bba890900189a01a97935d0ed102a91f503
Laurent V <112149608+LaurentDIA16@users.noreply.github.com>
2023-03-15 15:49:07 +0400

    Update classifierAddNewImage.py
    Supprimer informations training_endpoint, training_key, prediction_endpoint, prediction_key et prediction_resource_id

--- src/classifier/classifierAddNewImage.py
+++ src/classifier/classifierAddNewImage.py
@@ -5,11 +5,11 @@ from msrest.authentication import ApiKeyCredentials
 import os, time
 
 #load the keys and endpoints from the configuration file
-training_endpoint='https://westeurope.api.cognitive.microsoft.com/'
-training_key='ea64d9ef43e245d6bd6d719b6420e798'
-prediction_endpoint='https://westeurope.api.cognitive.microsoft.com/'
-prediction_key='ea64d9ef43e245d6bd6d719b6420e798'
-prediction_resource_id='/subscriptions/b5e3ab84-dbf9-4e8a-81d2-91337105a5e5/resourceGroups/GR16laurent/providers/Microsoft.CognitiveServices/accounts/Classifier'
+training_endpoint=''
+training_key=''
+prediction_endpoint=''
+prediction_key=''
+prediction_resource_id=''
 
 #Authenticate the client
 credentials = ApiKeyCredentials(in_headers={"Training-key": training_key})
@@ -18,7 +18,7 @@ prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": predict
 predictor = CustomVisionPredictionClient(prediction_endpoint, prediction_credentials)
 
 #Select the project id
-project_id = 'f3bb890c-270f-43cf-b3ec-daba5014b6a6'
+project_id = ''
 
 # Create tags
 tiramisu_tag = next(t for t in trainer.get_tags(project_id) if t.name == "Tiramisu")
